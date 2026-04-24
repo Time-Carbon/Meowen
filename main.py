58956ba342353d111aad6738234999b57a377166
Time <timejim20205@gmail.com>
2026-04-24 19:04:36 +0800

    modified:   .gitignore 	modified:   main.py 	modified:   pyproject.toml 	modified:   uv.lock

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
-def main():
-    print("Hello from unsloth-qwen3-5!")
+from unsloth import FastLanguageModel
+import torch
+
+max_context = 4096
+model_path = "./model/"
+
+def import_model(max_context, path):
+    
+    model, tokenizer = FastLanguageModel.from_pretrained(
+            model_name = path,
+            max_seq_length = max_context,
+            dtype = None,
+            load_in_4bit = True,
+            use_gradient_checkpointing = "unsloth",
+            gpu_memory_utilization = 0.8
+            )
+    
+    return model, tokenizer
+
+def create_lora(model, rank, lora_list):
+
+    lora = FastLanguageModel.get_peft_model(
+            model = model,
+            r = rank,
+            target_modules = lora_list,
+            lora_alpha = 2 * rank,
+            lora_dropout=0.0,
+            bias='none',
+            use_gradient_checkpointing='unsloth'
+            )
+
+    return lora
 
 
 if __name__ == "__main__":
-    main()
+
+    model, tokenizer = import_model(max_context, model_path)
+
+    lora_list = [
+            'q_proj',
+            'k_proj',
+            'v_proj',
+            'o_proj',
+            'gate_proj',
+            'up_proj',
+            'down_proj'
+            ]
+    create_lora(model, 8, lora_list)
