599ff712118b91ac39139ad1ce494cedcae9375b
Time <timejim20205@gmail.com>
2026-04-25 23:21:23 +0800

    modified:   .gitignore 	modified:   main.py 	modified:   uv.lock

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
+### Unsloth related
 from unsloth import FastLanguageModel
+from unsloth import is_bfloat16_supported
 import torch
 
+### Dataset related
+from datasets import load_dataset
+
+### Train related
+from trl import SFTTrainer
+from trl import GRPOTrainer, GRPOConfig
+from transformers import TrainingArguments
+
+### Global data
 max_context = 4096
 model_path = "./model/"
+lora_path = "./lora/"
 
-def import_model(max_context, path):
+### Main process
+def import_model():
     
     model, tokenizer = FastLanguageModel.from_pretrained(
-            model_name = path,
+            model_name = model_path,
             max_seq_length = max_context,
             dtype = None,
             load_in_4bit = True,
             use_gradient_checkpointing = "unsloth",
-            gpu_memory_utilization = 0.8
+            gpu_memory_utilization = 0.8,
+            fast_inference = False
             )
     
     return model, tokenizer
@@ -31,10 +45,40 @@ def create_lora(model, rank, lora_list):
 
     return lora
 
+def SFTtrain(lora, tokenizer, dataset, steps, lr, regularization):
+
+    trainer = SFTTrainer(
+            model = lora,
+            tokenizer = tokenizer,
+            train_dataset = dataset,
+            dataset_text_field = "text",
+            max_seq_length = max_context,
+            packing = False,
+            args = TrainingArguments(
+                per_device_train_batch_size = 2,
+                gradient_accumulation_steps = 4,
+                warmup_steps = 0.1 * steps,
+                max_steps = steps,
+                learning_rate = lr,
+                fp16 = not is_bfloat16_supported(),
+                bf16 = is_bfloat16_supported(),
+                logging_steps = 0.1 * steps,
+                optim = "adamw_8bit",
+                weight_decay = regularization,
+                lr_scheduler_type = "linear"
+                )
+            )
+
+    return lora
+
+def save_lora(lora, tokenizer):
+
+    lora.save_pretrained(lora_path)
+    tokenizer.save_pretrained(lora_path)
 
 if __name__ == "__main__":
 
-    model, tokenizer = import_model(max_context, model_path)
+    model, tokenizer = import_model()
 
     lora_list = [
             'q_proj',
@@ -45,4 +89,4 @@ if __name__ == "__main__":
             'up_proj',
             'down_proj'
             ]
-    create_lora(model, 8, lora_list)
+    lora = create_lora(model, 8, lora_list)
