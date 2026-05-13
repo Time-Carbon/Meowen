30c64dc7f6729ba0ee3aa507a017150a616f7468
Time <timejim20205@gmail.com>
2026-05-13 12:14:38 +0800

    modified:   script/download_model.sh

--- script/download_model.sh
+++ script/download_model.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-modelscope download --model Qwen/Qwen3.5-4B-Base --local_dir ./model
+modelscope download --model ABABA123/Qwen3.5-0.8B-Base-bnb-8bit --local_dir ./model
 modelscope download --token $TOKEN --dataset ABABA123/porn_text_raw --local_dir ./dataset
