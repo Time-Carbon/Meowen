26182c3623dae6464d53ae2db683d7b60f0b7541
Time <timejim20205@gmail.com>
2026-05-17 16:17:33 +0800

    modified:   unsloth_model/__init__.py

--- unsloth_model/__init__.py
+++ unsloth_model/__init__.py
@@ -6,7 +6,7 @@ os.environ["PYTORCH_ALLOC_CONF"] = "expandable_segments:True"
 import unsloth
 import torch
 
-torch.cuda.set_per_process_memory_fraction(0.8, device=0)
+torch.cuda.set_per_process_memory_fraction(1.0, device=0)
 
 from .trainer import *
 from .datasetLoader import *
