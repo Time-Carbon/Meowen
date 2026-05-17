f686362b7c65d2c950330408ed39e194d4ace8ab
Time <timejim20205@gmail.com>
2026-05-17 22:31:19 +0800

    new file:   .env.example 	modified:   script/download_model.sh

--- script/download_model.sh
+++ script/download_model.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+source .env
+
 modelscope download --model ABABA123/Qwen3.5-0.8B-Base-bnb-8bit --local_dir ./model
 modelscope download --token $TOKEN --dataset ABABA123/porn_text_raw --local_dir ./dataset
