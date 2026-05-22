d1984d8e3fd1cab281e39166cc01fb150814b38d
Time <timejim20205@gmail.com>
2026-05-23 01:33:12 +0800

    modified:   script/download_model.sh

--- script/download_model.sh
+++ script/download_model.sh
@@ -3,4 +3,4 @@
 source .env
 
 modelscope download --model ABABA123/Qwen3.5-0.8B-Base-bnb-8bit --local_dir ./model
-modelscope download --token $TOKEN --dataset ABABA123/porn_text_raw --local_dir ./dataset
+modelscope download --token $TOKEN --dataset ABABA123/porn_text_raw --local_dir ./dataset --include *.parquet
