59ec9418c1e26b8991f8ce762efe178ba211728a
TimeIsOnline <timejim20205@gmail.com>
2026-07-29 14:06:32 +0800

    modified:   unsloth_model/modelLoader/saveLoRA.py

--- unsloth_model/modelLoader/saveLoRA.py
+++ unsloth_model/modelLoader/saveLoRA.py
@@ -1,4 +1,7 @@
-def save_lora(lora, tokenizer, lora_path):
+def save_lora(lora, tokenizer, lora_path: str = "./lora"):
 
-    lora.save_pretrained(lora_path)
-    tokenizer.save_pretrained(lora_path)
+    # Save as safetensors format
+    lora.save_pretrained(lora_path + "safetensors/")
+    tokenizer.save_pretrained(lora_path + "safetensors/")
+    # Save as GGUF format
+    lora.save_pretrained_gguf(lora_path + "gguf/", tokenizer, quantization_method = "q8_0")
